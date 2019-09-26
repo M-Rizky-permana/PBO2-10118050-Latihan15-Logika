24adfbb9fa4e84980ae668ff6535ec88371cc4c3
M-Rizky-permana <55745444+M-Rizky-permana@users.noreply.github.com>
2019-09-26 20:17:41 +0700

    Add files via upload

---
+++ PBO210118050Latihan15Logika/src/pbo210118050latihan15logika/PBO210118050Latihan15Logika.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo210118050latihan15logika;
+
+/**
+ *
+ * @author 
+ * NAMA     : Muhamad Rizky Permana
+ * KELAS    : IF-2
+ * NIM      : 10118050
+ * Deskripsi Program : Menampilkan true or false
+ */
+public class PBO210118050Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+        
+        
+        
+    }
+    
+}
